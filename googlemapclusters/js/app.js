21d47a6bfc39056db3f82529eb7f41217ff0be7b
Vyas, Dhaval <dhaval.vyas@t4gkick.com>
2017-08-17 11:00:59 -0300

    Working example: Opens infoWindow on click of marker as well as Item from Marker List on left column.

--- googlemapclusters/js/app.js
+++ googlemapclusters/js/app.js
@@ -119,6 +119,15 @@ $(document).foundation();
 // // gmap.markers = [];
 // // gmap.infoWindow = null;
 var map;
+var infoWindow;
+
+var items = data.photos;
+var panel = $("#markerlist");
+var titleText;
+panel.innerHTML = '';
+
+var item = document.createElement('DIV');
+var title = document.createElement('A');
 
 function initMap() {
 
@@ -135,7 +144,7 @@ function initMap() {
 }
 
 function showmarkers() {
-    var infoWindow = new google.maps.InfoWindow();
+    
 
     // Create an array of alphabetical characters used to label the markers.
     // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -152,75 +161,60 @@ function showmarkers() {
         });
         
         marker.addListener('click', function() {
-            var title = item.photo_title;
-            var url = item.photo_url;
-            var fileurl = item.photo_file_url;
-
-            var infoHtml = '<div class="info"><h3>' + title +
-            '</h3><div class="info-body">' +
-            '<a href="' + url + '" target="_blank"><img src="' +
-            fileurl + '" class="info-img"/></a></div>' +
-            '<a href="http://www.panoramio.com/" target="_blank">' +
-            '<img src="http://maps.google.com/intl/en_ALL/mapfiles/' +
-            'iw_panoramio.png"/></a><br/>' +
-            '<a href="' + item.owner_url + '" target="_blank">' + item.owner_name +
-            '</a></div></div><div><strong>Location:</strong><br/>Latitude: '+item.latitude+',<br/>Longitude: '+item.longitude+'</div>';
-            infoWindow.setContent(infoHtml);
-            // infoWindow.setPosition(location.latitude, location.longitude);
-            infoWindow.open(map, marker);
+            markerclick(item, mylatlng, marker);
         });
+
+
+        titleText = item.photo_title;
+
+        title.href = '#';
+        title.className = 'title';
+        title.innerHTML = titleText;
+
+        if (titleText === '')
+        {
+            titleText = 'No title';
+        }
+        panel.append("<div><a id='item-"+i+"' href='#' class="+title.className+">"+titleText+"</a></div>");
+        // locations[i] = {lat: items[i].latitude, lng: items[i].longitude};
+
+        document.getElementById("item-"+i).addEventListener("click", function(){
+            markerclick(item, mylatlng, marker);
+        });
+
+        // title.addEventListener('click', function(){
+        //     console.log("hello");
+        //     markerclick(item, mylatlng, marker);
+        // });
+        
+        i++;
+        // console.log(i);
+
         return marker;
     });
-
+    infoWindow = new google.maps.InfoWindow();
     // Add a marker clusterer to manage the markers.
     var markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
-// var locations = [
-//     {lat: -31.563910, lng: 147.154312},
-//     {lat: -33.718234, lng: 150.363181},
-//     {lat: -33.727111, lng: 150.371124},
-//     {lat: -33.848588, lng: 151.209834},
-//     {lat: -33.851702, lng: 151.216968},
-//     {lat: -34.671264, lng: 150.863657},
-//     {lat: -35.304724, lng: 148.662905},
-//     {lat: -36.817685, lng: 175.699196},
-//     {lat: -36.828611, lng: 175.790222},
-//     {lat: -37.750000, lng: 145.116667},
-//     {lat: -37.759859, lng: 145.128708},
-//     {lat: -37.765015, lng: 145.133858},
-//     {lat: -37.770104, lng: 145.143299},
-//     {lat: -37.773700, lng: 145.145187},
-//     {lat: -37.774785, lng: 145.137978},
-//     {lat: -37.819616, lng: 144.968119},
-//     {lat: -38.330766, lng: 144.695692},
-//     {lat: -39.927193, lng: 175.053218},
-//     {lat: -41.330162, lng: 174.865694},
-//     {lat: -42.734358, lng: 147.439506},
-//     {lat: -42.734358, lng: 147.501315},
-//     {lat: -42.735258, lng: 147.438000},
-//     {lat: -43.999792, lng: 170.463352}
-// ]
 
-var items = data.photos;
-var panel = $("#markerlist");
-panel.innerHTML = '';
+function markerclick(item, latlng, marker) {
+    
+    var title = item.photo_title;
+    var url = item.photo_url;
+    var fileurl = item.photo_file_url;
 
-var item = document.createElement('DIV');
-var title = document.createElement('A');
-title.href = '#';
-title.className = 'title';
-title.innerHTML = titleText;
-var locations = [];
-
-
-for(var i =0; i < items.length; i++)
-    {
-    var titleText = items[i].photo_title;
-    if (titleText === '')
-    {
-        titleText = 'No title';
-    }
-    panel.append("<div><a href='#'>"+titleText+"</a></div>");
-    locations[i] = {lat: items[i].latitude, lng: items[i].longitude};
+    var infoHtml = '<div class="info"><h3>' + title +
+    '</h3><div class="info-body">' +
+    '<a href="' + url + '" target="_blank"><img src="' +
+    fileurl + '" class="info-img"/></a></div>' +
+    '<a href="http://www.panoramio.com/" target="_blank">' +
+    '<img src="http://maps.google.com/intl/en_ALL/mapfiles/' +
+    'iw_panoramio.png"/></a><br/>' +
+    '<a href="' + item.owner_url + '" target="_blank">' + item.owner_name +
+    '</a></div></div><div><strong>Location:</strong><br/>Latitude: '+item.latitude+',<br/>Longitude: '+item.longitude+'</div>';
+    infoWindow.setContent(infoHtml);
+    infoWindow.setPosition(latlng);
+    infoWindow.open(map, marker);
+    
 }
